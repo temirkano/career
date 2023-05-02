983322ca192b3a2c41dcd8ade4656205ac870d2d
Begaiym <imianovabegaiym02@gmail.com>
2023-05-01 17:01:07 -1200

    feat/main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import DownloadApp from "./components/DownloadApp/DownloadApp";
-import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 // import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <div className="App">
       {/* <AboutPage /> */}
-      <DownloadApp />
+      <Main />
     </div>
   );
 }
