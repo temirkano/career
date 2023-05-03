4706de6b9f744b9602714a45db6c7c7250443e83
Begaiym <imianovabegaiym02@gmail.com>
2023-05-02 15:17:06 -1200

    feat:main

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <AboutPage /> */}
-      {/* <Main /> */}
-      <DownloadApp />
+      <Main />
+      {/* <DownloadApp /> */}
     </div>
   );
 }
