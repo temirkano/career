b012d534422205d44213e285955522063f3dd68d
Begaiym <imianovabegaiym02@gmail.com>
2023-05-16 05:46:12 -1200

    fix:main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import LatestNews from "./components/LatestNews/LatestNews";
-import GlobalConnections from "./components/GlobalConnections/GlobalConnections";
-import LatestNewsCard from "./components/LatestNews/LatestNewsCard/LatestNewsCard";
 import LoveWorking from "./components/LoveWorking/LoveWorking";
 
 // import AboutPage from "./pages/AboutPage";
 function App() {
   return (
     <div className="App">
-      {/* <AboutPage /> */}
-      {/* <GlobalConnections /> */}
-      {/* <LatestNews /> */}
       <LoveWorking />
     </div>
   );
