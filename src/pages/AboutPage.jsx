75fec4af349b3d5541016fbef20c56dfc898d3c9
Ani25ani <anjarsuper51@gmail.com>
2023-05-17 21:30:18 +0600

    feat/detalis-jobDes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,6 +3,7 @@ import Main from "../components/Main/Main";
 import Clients from "../components/OurClients/Clients";
 import DownloadApp from "../components/DownloadApp/DownloadApp";
 import Footer from "../components/Footer/Footer";
+import JobDes from "../components/JobDes/JobDes";
 
 function AboutPage() {
   return (
@@ -11,6 +12,7 @@ function AboutPage() {
       <Clients />
       <DownloadApp />
       <Footer />
+      <JobDes/>
     </>
   );
 }
