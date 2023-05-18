7f47439193d4d1d42d37129fe79634378ff4e218
Begaiym <imianovabegaiym02@gmail.com>
2023-05-17 17:21:35 -1200

    fix:main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
 import LoveWorking from "./components/LoveWorking/LoveWorking";
-import Sales from "./components/Sales/Sales";
-import LatestNews from "./components/LatestNews/LatestNews";
-
-// import AboutPage from "./pages/AboutPage";
 function App() {
   return (
     <div className="App">
       <LoveWorking />
-      {/* <Sales /> */}
-      {/* <LatestNews /> */}
     </div>
   );
 }
--- src/components/Sales/Sales.jsx
+++ src/components/Sales/Sales.jsx
@@ -3,8 +3,9 @@ import vector from "../../assets/SalesImg/Vector.svg";
 import baggage from "../../assets/SalesImg/baggage.svg";
 import eye from "../../assets/SalesImg/eye.svg";
 import pin from "../../assets/SalesImg/pin.svg";
-import like from "../../assets/SalesImg/like.svg";
-
+import { ReactComponent as BoxIcon } from "../../assets/SalesImg/like.svg";
+import { ReactComponent as CheckIcon } from "../../assets/SalesImg/likeRed.svg";
+import { useEffect, useState } from "react";
 
 function Sales() {
   return (
@@ -12,19 +13,19 @@ function Sales() {
       <div className={scss.container}>
         <div className={scss.rightSales}>
           <div className={scss.link}>
-            <a href="">Home</a>
+            <a href="/#">Home</a>
             <img src={vector} alt="vector" />
-            <a href="">Jobs</a>
+            <a href="/#">Jobs</a>
             <img src={vector} alt="vector" />
-            <a>Category</a>
+            <a href="/#">Category</a>
             <img src={vector} alt="vector" />
-            <a className={scss.sls}>President of Sales</a>
+            <a href="/#" className={scss.sls}>President of Sales</a>
           </div>
           <h1>President of Sales</h1>
           <div className={scss.dataBoxs}>
             <div className={scss.dataBox}>
               <img src={baggage} alt="baggageIcon" />
-              <a href="">Marketing</a>
+              <a href="/#">Marketing</a>
             </div>
             <div className={scss.dataBox}>
               <img src={eye} alt="eyeIcon" />
@@ -39,7 +40,6 @@ function Sales() {
         <div className={scss.leftSales}>
           <div className={scss.checkBox}>
             <button>Apply Now</button>
-            <img src={like} alt="likeIcon" />
           </div>
           <p>Expiration date:
             <span> 19 May 2026</span>
